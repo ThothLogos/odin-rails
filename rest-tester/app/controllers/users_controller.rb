a9b4f39a7c95a65ec57eec45907fbf06c69e8987
ThothLogos <naughton.joshua@gmail.com>
2015-03-07 18:44:01 -0500

    Finish controller with create method, add views

--- rest-tester/app/controllers/users_controller.rb
+++ rest-tester/app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
 
-  
+  def index
+    @name = "Index action"
+  end
+
+  def new
+    @name = "Signup"
+  end
+
+  def edit
+    @name = "user edit"
+  end
+
+  def show
+    @name = "Show user"
+  end
+
+  def create
+    @name = "Create user"
+  end
   
 end
